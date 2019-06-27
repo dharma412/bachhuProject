647ac2f07b796378fa94e46e5b6ac7d4881ac674
Bachhu456 <iambachhu@gmail.com>
2019-06-27 12:31:47 +0530

    Add code in test_home

--- test/regression/test_homePage.py
+++ test/regression/test_homePage.py
@@ -1,6 +1,6 @@
 from selenium.webdriver import Chrome
 from lib.ui.pom.homePage import HomePage
-from lib.utils.mouseHover import moveToElement
+#from lib.utils.mouseHover import moveToElement
 import unittest
 import pytest
 import time
@@ -16,24 +16,24 @@ class Test_homePage01(unittest.TestCase):
 
     def test_homePage_valid_tc01(self):
         #self.login_page.wait_for_loginPage()
-        self.home_page.get_user_name().send_keys("")
-        self.home_page.get_user_passwor().send_keys("")
+        self.home_page.get_user_name().send_keys("bachhunandi38@gmail.com")
+        self.home_page.get_user_passwor().send_keys("electronices2#")
         self.home_page.get_user_loginButton().click()
         actual_result=self.browser.title
 
         print(actual_result)
         ex_msg="Facebook"
         self.assertTrue(self.browser.title,'Facebook')
-        #self.browser.find_element_by_xpath("//div[@id='userNavigationLabel']").click
-        logout=self.browser.find_element_by_id("userNavigationLabel")
-        moveToElement(self.browser,logout)
+        '''#self.browser.find_element_by_xpath("//div[@id='userNavigationLabel']").click
+        #logout=self.browser.find_element_by_id("userNavigationLabel")
+        #moveToElement(self.browser,logout)
 
         time.sleep(35)
         logout1=self.browser.find_element_by_partial_link_text("Log Out")
         logout1.click()
 
 
-        #self.browser.find_element_by_partial_link_text('Log Out').click()
+        #self.browser.find_element_by_partial_link_text('Log Out').click()'''
 
 
 
